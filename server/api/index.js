3f4109fca7f423a5c59a448fc5ace02b23d17a73
Rishabh Yadav <yrishabh325@gmail.com>
2025-02-21 12:42:07 +0530

    Deploying

---
+++ server/api/index.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connectDB = require('../utils/db');
+const transactionsRoute = require('../router/transaction-router')
+
+
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); 
+
+// app.use(errorMiddleware)
+ 
+
+// Routes
+app.use('/api/transactions', transactionsRoute);
+
+
+// Starting the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
